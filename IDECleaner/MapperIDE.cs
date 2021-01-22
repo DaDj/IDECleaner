14c6193e530c1f94958a00298f7da790f23c9fc6
Patrick Gramann <patgramann@gmail.com>
2021-01-22 19:14:51 +0100

    first

---
+++ IDECleaner/MapperIDE.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDECleaner
+{
+    public class MapperIDE
+    {
+        public int ModelId { get; }
+        public string ModelName { get; set; }
+        public string Texture { get; }
+
+        public MapperIDE(string modelName)
+        {
+            ModelName = modelName;
+        }
+
+        public MapperIDE(string[] data)
+        {
+            ModelId = Int32.Parse(data[0]);
+            ModelName = data[1].Trim();
+            Texture = data[2].Trim();
+        }
+
+        public static List<MapperIDE> ParseMultipleIdes(List<string> data)
+        {
+            return data
+                .Select(line => line.TrimStart().Split(','))
+                .Where(ide => ide.Length > 2 && !ide.First().StartsWith("#"))
+                .Select(ide => new MapperIDE(ide))
+                .ToList();
+        }
+    }
+}
